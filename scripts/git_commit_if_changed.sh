4eda108849ee7a3519cdea952fcf018bb42af278
Per Jensen <per2jensen@gmail.com>
2026-05-25 09:48:40 +0200

    release and refresh workflows now save sbom and sarif files in repo

--- scripts/git_commit_if_changed.sh
+++ scripts/git_commit_if_changed.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# Usage: scripts/git_commit_if_changed.sh <path> <message>
-git add "$1"
-if ! git diff --cached --quiet "$1"; then
-  git commit -m "$2"
+# Usage: scripts/git_commit_if_changed.sh <message> <path1> [path2] ...
+MESSAGE="$1"
+shift
+
+git add "$@"
+if ! git diff --cached --quiet; then
+  git commit -m "$MESSAGE"
 fi
